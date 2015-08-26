15877af28024af7b88677c33f9bb61c5d3d29b15
Artur Ptaszek <artur.ptaszek@gmail.com>
2015-08-26 15:47:50 +0200

    #1 npm up packages. Add sample state elements

--- clientReact/src/components/utils/Authenticated.js
+++ clientReact/src/components/utils/Authenticated.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { RouteHandler } from 'react-router';
 
 class Authenticated extends React.Component {
-    static willTransitionFrom() {
-        console.log('asdfasdf'); // eslint-disable-line
-    }
-
     static willTransitionTo(transition) {
         if(window.localStorage.getItem('is-auth') !== 'true') {
             transition.redirect('/', {});// eslint-disable-line
@@ -15,7 +11,8 @@ class Authenticated extends React.Component {
     render() {
         return (
             <div>
-                <h1>Authenticated View</h1>
+                <h1>Authgfhgfhgfghf View</h1>
+                <div>hgfhfhjfjhfjhgjh</div>
                 <RouteHandler/>
             </div>
         );
--- clientReact/src/components/views/MainView.js
+++ clientReact/src/components/views/MainView.js
@@ -3,11 +3,35 @@ import { AppBar } from 'material-ui';
 
 
 class MainView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            timer: 0
+        };
+    }
+
+    tick() {
+        this.setState({
+            timer: this.state.timer + 10
+        });
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(this.tick.bind(this), 200);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+    dupa(){
+        console.log('asdfasdfsdf ');//eslint-disable-line
+    }
     render() {
         return (
             <div>
+                <h1>{this.state.timer}</h1>
                 <AppBar
                     title="Title"
+                    onLeftIconButtonTouchTap={this.dupa}
                     iconClassNameRight="muidocs-icon-navigation-expand-more" />
             </div>
         );
