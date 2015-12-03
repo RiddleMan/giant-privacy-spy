4b0b4ff7fd71460c7d4f1757b3a5e3e792a9338b
Artur Ptaszek <artur.ptaszek@gmail.com>
2015-12-04 00:13:36 +0100

    Material UI added

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 const content = document.getElementById('content');
 
